d72e744677212c510c56ae69e418d6025bb86b62
ebbahansson <ebbahan@gmail.com>
2026-04-24 16:36:09 +0200

    Update script and add more Hanna images

--- script.js
+++ script.js
@@ -20,20 +20,20 @@ const gameData = {
             name: "BRÖLLOPSTAJM",
             questions: [
                 { value: 100, question: "Hur många dagar är det kvar till bröllopet?", answer: "70 dagar kvar!", image: "./Bilder/Hanna/Frågor/bröllopspardom.png" },
-                { value: 200, question: "UTMANING: Brainstorma den perfekta bröllopshashtaggen till Hanna och Per - motivera ert val!", answer: "#Bröllop2026??", image: "./Bilder/Hanna/Frågor/hashtag3.png" },
-                { value: 300, question: "Hanna och Per ska gifta sig på Stora Holm Säteri. Men vad är ett säteri egentligen?", answer: "En större herrgård. Historiskt sett ägdes och beboddes den av en adelsman", image: "./Bilder/Hanna/Frågor/storaholm.png" },
+                { value: 200, question: "UTMANING: På bröllop vill man ju vara extra fin i håret, eller hur? Utmana ett annat lag och skapa varsin håruppsättning på en lagmedlem – använd det som finns tillgängligt. Ju galnare, mer kreativ och överdriven frisyr, desto bättre! Hanna utser vinnaren.", answer: "💇‍♀️", image: "./Bilder/Hanna/Frågor/hannalugg.png" },
+                { value: 300, question: "Hanna och Per ska gifta sig på Stora Holm Säteri. Men vad är ett säteri egentligen?", answer: "En större herrgård. Historiskt sett ägdes och beboddes den av en adelsman.", image: "./Bilder/Hanna/Frågor/storaholm.png" },
                 { value: 400, question: "När man gifter sig ska man ha nått av varje - i vilka kategorier? ", answer: "Något nytt, något gammalt, något lånat, något blått", image: "./Bilder/Hanna/Frågor/ringar.jpg" },
-                { value: 500, question: "tbh", answer: "Svar här", image: null }
+                { value: 500, question: "Innan Per kom in i bilden fanns det tydligen en annan kille som fångade Hannas uppmärksamhet. Hur gammal var Hanna här?", answer: "17 år", image: "./Bilder/Hanna/Frågor/hannakollar.jpg" }
             ]
         },
         {
             
             name: "PER",
             questions: [
-                { value: 100, question: "Hur lång är Per? Svara i cm.", answer: "Svar här", image: "./Bilder/Hanna/Frågor/perlång.png" },
-                { value: 200, question: "Hur gammal var Per när han och Hanna träffades?", answer: "Svar här", image: "./Bilder/Hanna/Frågor/perliten.jpg" },
-                { value: 300, question: "När Per är bakis, vad beställer han på pizzerian?", answer: "Svar här", image: "./Bilder/Hanna/Frågor/perbakis.jpg" },
-                { value: 400, question: "Hur friade Per till Hanna?", answer: "Svar här", image: "./Bilder/Hanna/Frågor/perskål.jpg" },
+                { value: 100, question: "Hur lång är Per? Svara i cm.", answer: "202 cm", image: "./Bilder/Hanna/Frågor/perlång.png" },
+                { value: 200, question: "Hur gammal var Per när han och Hanna träffades?", answer: "29 år (13 Nov 2015)", image: "./Bilder/Hanna/Frågor/perliten.jpg" },
+                { value: 300, question: "När Per är bakis, vad beställer han på pizzerian?", answer: "Calzone", image: "./Bilder/Hanna/Frågor/perbakis.jpg" },
+                { value: 400, question: "Hur friade Per till Hanna?", answer: "När hon var som svagast. Hon låg utslagen och bakfull på soffan och jag ställde mig på knä. Hade köpt ”frågeringar” som brände i fickan och kunde inte hålla sig till något vackrare tillfälle.", image: "./Bilder/Hanna/Frågor/perskål.jpg" },
                 { value: 500, question: "UTMANING: Iscensätt frieriet.", answer: "👀", image: "./Bilder/Hanna/Frågor/hannaper2.jpg" }
             ]
         },
@@ -41,10 +41,10 @@ const gameData = {
             name: "HANNA",
             questions: [
                 { value: 100, question: "Vilken är Hannas mest använda Emoji?", answer: "Hanna får visa!", image: "./Bilder/Hanna/Frågor/mobil.jpg" },
-                { value: 200, question: "Vad var Hannas allra första jobb?", answer: "Städare på ett hotell i Kristianstad", image: "./Bilder/Hanna/Frågor/barn.jpg" },
+                { value: 200, question: "Vad var Hannas allra första jobb?", answer: "Städare på hotell Christian IV i Kristianstad. Hon berättar gärna om när hon träffade Brolle Jr där.", image: "./Bilder/Hanna/Frågor/barn.jpg" },
                 { value: 300, question: "UTMANING: Hanna är ju en riktigt läcker ingenjör. Utmana ett annat lag och bygg ett så högt torn som möjligt!", answer: "Högst torn vinner poängen!", image: "./Bilder/Hanna/Frågor/hannaingenjör.jpg" },
                 { value: 400, question: "Vad heter Hannas föräldrar? (Bonus: vad heter hennes bröder?)", answer: "Catharina & Jesper! (Tvillingarna heter Johan och Nils)", image: "./Bilder/Hanna/Frågor/familj.png" },
-                { value: 500, question: "Hanna gillar fordon av olika slag – men vilka körkort har hon? (Ange bokstäver)", answer: "Svar här", image: "./Bilder/Hanna/Frågor/fordon.jpg" }
+                { value: 500, question: "Hanna gillar fordon av olika slag – men vilka körkort har hon?", answer: "B, C", image: "./Bilder/Hanna/Frågor/fordon.jpg" }
             ]
         },
         {
@@ -52,7 +52,7 @@ const gameData = {
             questions: [
                 { value: 100, question: "Vilket är Sveriges vanligaste lövträd?", answer: "Björk", image: "./Bilder/Hanna/Frågor/träd.jpg" },
                 { value: 200, question: "Vad behövs för att ved ska brinna bra? Nämn minst tre saker.", answer: "Syre, torr ved, lämplig storlek, tändkälla", image: "./Bilder/Hanna/Frågor/eld.jpg" },
-                { value: 300, question: "Familjen Löved bor på Odensvägen i Sävedalen – vilket år köpte de huset?", answer: "Svar här", image: "./Bilder/Hanna/Frågor/sävedalen.jpg" },
+                { value: 300, question: "Familjen Löved bor på Odensvägen i Sävedalen – vilket år köpte de huset?", answer: "2024", image: "./Bilder/Hanna/Frågor/sävedalen.jpg" },
                 { value: 400, question: "UTMANING: Skriv en kärleksdikt från Hanna till Per. Orden löv och ved måste vara med. Ni har 2 minuter!", answer: "👀", image: "./Bilder/Hanna/Frågor/hannaper1.jpg" },
                 { value: 500, question: "Det sägs att det finns 5 Löve(d) languages. Nämn dem! (Bonus: vilket är Hannas kärleksspråk?)", answer: "Bekräftande ord, kvalitetstid, gåvor, tjänster och fysisk beröring", image: "./Bilder/Hanna/Frågor/hjärta.jpg" }
             ]
@@ -60,11 +60,11 @@ const gameData = {
         {
             name: "VAD SKER?",
             questions: [
-                { value: 100, question: "Vad äter Hanna egentligen på bilden?", answer: "Svar här", image: "./Bilder/Hanna/Frågor/anka.png" },
-                { value: 200, question: "Varför var Hanna utklädd till en pudel?", answer: "Svar här", image: "./Bilder/Hanna/Frågor/pudel.jpg" },
+                { value: 100, question: "Vad sker här???!!!", answer: "Hanna är på Peace and Love i Borlänge, året är 2011 och de 'hänger med tältgrannarna'.", image: "./Bilder/Hanna/Frågor/grädde.jpg" },
+                { value: 200, question: "UTMANING! Vem skulle tacka nej till något gott i glaset? Inte Hanna i alla fall! Utmana ett annat lag och blanda ihop varsin drink till Hanna – använd det som finns tillgängligt. När kreationerna är klara får Hanna provsmaka och kora vinnaren: godast (eller mest kreativa!) tar hem poängen.", answer: "🍻", image: "./Bilder/Hanna/Frågor/hannaöl.jpg" },
                 { value: 300, question: "Var är Hanna på bilden?", answer: "Way Out West 2012", image: "./Bilder/Hanna/Frågor/wow.jpg" },
                 { value: 400, question: "Vilken låt spelade Hanna mest troligt här?", answer: "Wonderwall?", image: "./Bilder/Hanna/Frågor/gitarr.jpg" },
-                { value: 500, question: "UTMANING: Hitta på händelseförloppet som skedde precis innan denna bilden togs! Var är det? När är det? Varför har hon grädde i hela ansiktet?", answer: "👀", image: "./Bilder/Hanna/Frågor/grädde.jpg" }
+                { value: 500, question: "Varför var Hanna utklädd till en pudel?", answer: "Vi skulle på fest", image: "./Bilder/Hanna/Frågor/pudel.jpg"  }
             ]
         }
     ]
