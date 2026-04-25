ed8b6078c3ee1c5bf2df603d8d39069e3f38413c
ebbahansson <ebbahan@gmail.com>
2026-04-25 09:49:10 +0200

    kompletterat

--- script.js
+++ script.js
@@ -20,10 +20,9 @@ const gameData = {
             name: "BRÖLLOPSTAJM",
             questions: [
                 { value: 100, question: "Hur många dagar är det kvar till bröllopet?", answer: "70 dagar kvar!", image: "./Bilder/Hanna/Frågor/bröllopspardom.png" },
-                { value: 200, question: "UTMANING: På bröllop vill man ju vara extra fin i håret, eller hur? Utmana ett annat lag och skapa varsin håruppsättning på en lagmedlem – använd det som finns tillgängligt. Ju galnare, mer kreativ och överdriven frisyr, desto bättre! Hanna utser vinnaren.", answer: "💇‍♀️", image: "./Bilder/Hanna/Frågor/hannalugg.png" },
-                { value: 300, question: "Hanna och Per ska gifta sig på Stora Holm Säteri. Men vad är ett säteri egentligen?", answer: "En större herrgård. Historiskt sett ägdes och beboddes den av en adelsman.", image: "./Bilder/Hanna/Frågor/storaholm.png" },
-                { value: 400, question: "När man gifter sig ska man ha nått av varje - i vilka kategorier? ", answer: "Något nytt, något gammalt, något lånat, något blått", image: "./Bilder/Hanna/Frågor/ringar.jpg" },
-                { value: 500, question: "Innan Per kom in i bilden fanns det tydligen en annan kille som fångade Hannas uppmärksamhet. Hur gammal var Hanna här?", answer: "17 år", image: "./Bilder/Hanna/Frågor/hannakollar.jpg" }
+                { value: 200, question: "UTMANING: På bröllop vill man ju vara extra fin i håret. Gör en bröllopsuppsättning på Hanna! Tema: EXTRA ALLT. Ni har 3 minuter på er.", answer: "💇‍♀️", image: "./Bilder/Hanna/Frågor/hannalugg.jpg" },
+                { value: 300, question: "Detta är kanske chockerande för er alla men Hanna är faktiskt redan gift. Bröllopet skedde i \"målarrummet\" med en man som hette Anton, enligt säkra källor. Men när var detta historiska bröllop?", answer: "Dagis", image: "./Bilder/Hanna/Frågor/grädde.jpg" },
+                { value: 400, question: "Att hitta bröllopsklänningar kan vara en djungel. Tyvärr finns bara dessa tre kvar i affären, vilken skulle Hanna ha valt?", answer: "👗", image: "./Bilder/Hanna/Frågor/klanningkombo.png"}
             ]
         },
         {
@@ -31,40 +30,36 @@ const gameData = {
             name: "PER",
             questions: [
                 { value: 100, question: "Hur lång är Per? Svara i cm.", answer: "202 cm", image: "./Bilder/Hanna/Frågor/perlång.png" },
-                { value: 200, question: "Hur gammal var Per när han och Hanna träffades?", answer: "29 år (13 Nov 2015)", image: "./Bilder/Hanna/Frågor/perliten.jpg" },
-                { value: 300, question: "När Per är bakis, vad beställer han på pizzerian?", answer: "Calzone", image: "./Bilder/Hanna/Frågor/perbakis.jpg" },
-                { value: 400, question: "Hur friade Per till Hanna?", answer: "När hon var som svagast. Hon låg utslagen och bakfull på soffan och jag ställde mig på knä. Hade köpt ”frågeringar” som brände i fickan och kunde inte hålla sig till något vackrare tillfälle.", image: "./Bilder/Hanna/Frågor/perskål.jpg" },
-                { value: 500, question: "UTMANING: Iscensätt frieriet.", answer: "👀", image: "./Bilder/Hanna/Frågor/hannaper2.jpg" }
+                { value: 200, question: "När Per är bakis, vad beställer han på pizzerian?", answer: "Calzone", image: "./Bilder/Hanna/Frågor/perbakis.jpg"},
+                { value: 300, question: "Hur friade Per till Hanna?", answer: "När hon var som svagast. Hon låg utslagen och bakfull på soffan och jag ställde mig på knä. Hade köpt ”frågeringar” som brände i fickan och kunde inte hålla sig till något vackrare tillfälle.", image: "./Bilder/Hanna/Frågor/perskål.jpg"},
+                { value: 400, question: "UTMANING: Iscensätt frieriet.", answer: "👀", image: "./Bilder/Hanna/Frågor/hannaper2.jpg" }
             ]
         },
         {
             name: "HANNA",
             questions: [
                 { value: 100, question: "Vilken är Hannas mest använda Emoji?", answer: "Hanna får visa!", image: "./Bilder/Hanna/Frågor/mobil.jpg" },
-                { value: 200, question: "Vad var Hannas allra första jobb?", answer: "Städare på hotell Christian IV i Kristianstad. Hon berättar gärna om när hon träffade Brolle Jr där.", image: "./Bilder/Hanna/Frågor/barn.jpg" },
-                { value: 300, question: "UTMANING: Hanna är ju en riktigt läcker ingenjör. Utmana ett annat lag och bygg ett så högt torn som möjligt!", answer: "Högst torn vinner poängen!", image: "./Bilder/Hanna/Frågor/hannaingenjör.jpg" },
-                { value: 400, question: "Vad heter Hannas föräldrar? (Bonus: vad heter hennes bröder?)", answer: "Catharina & Jesper! (Tvillingarna heter Johan och Nils)", image: "./Bilder/Hanna/Frågor/familj.png" },
-                { value: 500, question: "Hanna gillar fordon av olika slag – men vilka körkort har hon?", answer: "B, C", image: "./Bilder/Hanna/Frågor/fordon.jpg" }
+                { value: 200, question: "Hanna gillar fordon av olika slag – men vilka körkort har hon?", answer: "B, C", image: "./Bilder/Hanna/Frågor/fordon.jpg"},
+                { value: 300, question: "UTMANING: Hanna är ju en riktigt läcker ingenjör. Utmana Hanna och bygg ett så högt torn som möjligt!", answer: "Högst torn vinner poängen!", image: "./Bilder/Hanna/Frågor/hannaingenjör.jpg" },
+                { value: 400, question: "Vad heter Hannas föräldrar? (Bonus: vad heter hennes bröder?)", answer: "Catharina & Jesper! (Tvillingarna heter Johan och Nils)", image: "./Bilder/Hanna/Frågor/familj.png" }
             ]
         },
         {
             name: "LÖVED",
             questions: [
-                { value: 100, question: "Vilket är Sveriges vanligaste lövträd?", answer: "Björk", image: "./Bilder/Hanna/Frågor/träd.jpg" },
-                { value: 200, question: "Vad behövs för att ved ska brinna bra? Nämn minst tre saker.", answer: "Syre, torr ved, lämplig storlek, tändkälla", image: "./Bilder/Hanna/Frågor/eld.jpg" },
-                { value: 300, question: "Familjen Löved bor på Odensvägen i Sävedalen – vilket år köpte de huset?", answer: "2024", image: "./Bilder/Hanna/Frågor/sävedalen.jpg" },
-                { value: 400, question: "UTMANING: Skriv en kärleksdikt från Hanna till Per. Orden löv och ved måste vara med. Ni har 2 minuter!", answer: "👀", image: "./Bilder/Hanna/Frågor/hannaper1.jpg" },
-                { value: 500, question: "Det sägs att det finns 5 Löve(d) languages. Nämn dem! (Bonus: vilket är Hannas kärleksspråk?)", answer: "Bekräftande ord, kvalitetstid, gåvor, tjänster och fysisk beröring", image: "./Bilder/Hanna/Frågor/hjärta.jpg" }
+                { value: 100, question: "Från WinBLAD till LÖVed. Hanna har utöver växtinspirerande efternamn även gröna fingrar. Vad är det hon försöker odla på denna bilden från 2016? ", answer: "Avokado 🥑", image: "./Bilder/Hanna/Frågor/växt.jpg" },
+                { value: 200, question: "Det sägs att det finns 5 Löve(d) languages. Nämn dem! (Bonus: vilket är Hannas kärleksspråk?)", answer: "Bekräftande ord, kvalitetstid, gåvor, tjänster och fysisk beröring", image: "./Bilder/Hanna/Frågor/hjärta.jpg"},
+                { value: 300, question: "Familjen Löved bor på Odensvägen i Sävedalen – vilket år köpte de huset?", answer: "2024", image: "./Bilder/Hanna/Frågor/savedalen.jpg" },
+                { value: 400, question: "UTMANING: Skriv en kärleksdikt från Hanna till Per. Orden löv och ved måste vara med. Ni har 2 minuter!", answer: "👀", image: "./Bilder/Hanna/Frågor/hannaper1.jpg" }
             ]
         },
         {
             name: "VAD SKER?",
             questions: [
-                { value: 100, question: "Vad sker här???!!!", answer: "Hanna är på Peace and Love i Borlänge, året är 2011 och de 'hänger med tältgrannarna'.", image: "./Bilder/Hanna/Frågor/grädde.jpg" },
-                { value: 200, question: "UTMANING! Vem skulle tacka nej till något gott i glaset? Inte Hanna i alla fall! Utmana ett annat lag och blanda ihop varsin drink till Hanna – använd det som finns tillgängligt. När kreationerna är klara får Hanna provsmaka och kora vinnaren: godast (eller mest kreativa!) tar hem poängen.", answer: "🍻", image: "./Bilder/Hanna/Frågor/hannaöl.jpg" },
-                { value: 300, question: "Vad sker här???!!!", answer: "Way Out West 2012", image: "./Bilder/Hanna/Frågor/wow.jpg" },
-                { value: 400, question: "Vad sker här???!!!", answer: "Wonderwall?", image: "./Bilder/Hanna/Frågor/gitarr.jpg" },
-                { value: 500, question: "Vad sker här???!!!", answer: "Vi skulle på fest", image: "./Bilder/Hanna/Frågor/pudel.jpg"  }
+                { value: 100, question: "Vad sker här???!!! Vilket år???", answer: "Peace & Love, 2011", image: "./Bilder/Hanna/Frågor/gitarr.jpg" },
+                { value: 200, question: "UTMANING! Vem skulle tacka nej till något gott i glaset? Inte Hanna i alla fall! Utmana ett annat lag och blanda ihop varsin drink till Hanna – använd det som finns tillgängligt.", answer: "🍻", image: "./Bilder/Hanna/Frågor/hannaöl.jpg" },
+                { value: 300, question: "Vad sker här???!!! Vilket år???", answer: "Way Out West, 2012", image: "./Bilder/Hanna/Frågor/wow.jpg" },
+                { value: 400, question: "Vad sker här???!!! Vilket år???", answer: "Gymnasiefest, 2013", image: "./Bilder/Hanna/Frågor/pudel.jpg" }
             ]
         }
     ]
